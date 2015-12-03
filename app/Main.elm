94f7891a4166274cc97b4f3b7507c71eae117310
Kadi Kraman <kadi.kraman@red-badger.com>
2015-12-03 16:21:16 +0000

    Render a list of moods to choose from

--- app/Main.elm
+++ app/Main.elm
@@ -1,10 +1,18 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+moodList = [ "Amused", "Bewildered", "Dirty", "High" ]
+
+renderMoodList =
+  let
+    items = List.map (\mood -> li [ ] [ text mood ] ) moodList
+  in
+    ul [ class "mood-list" ] items
 
 main =
   div [ id "content" ]
     [
       h1 [ ] [ text "Fancy a drink?" ],
-      span [ class "intro"] [ text "Drinks based on your mood. Just select your mood and receive a drink recommendation just for you." ]
+      span [ class "intro"] [ text "Drinks based on your mood. Just select your mood and receive a drink recommendation just for you." ],
+      renderMoodList
     ]
